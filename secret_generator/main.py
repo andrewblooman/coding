f1584cd3c1ef945256f42314b342043d16ca1929
Andrew Blooman <39947655+andrewblooman@users.noreply.github.com>
2023-07-26 15:37:06 +0100

    local config

---
+++ secret_generator/main.py
@@ -0,0 +1,15 @@
+import secrets
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    length = int(input("Enter the length of the password: "))
+    password = generate_password(length)
+    print("Generated password:", password)
+
+if __name__ == "__main__":
+    main()
